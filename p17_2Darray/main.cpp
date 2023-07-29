42e2df7094f80be3a2c635763f2abf8384787ac1
Mahdi Movahedi <m.movahedimonfared@gmail.com>
2023-07-29 22:44:36 +0330

    progressing a step further with p17
    Signed-off-by: Mahdi Movahedi <m.movahedimonfared@gmail.com>

--- p17_2Darray/main.cpp
+++ p17_2Darray/main.cpp
@@ -6,7 +6,7 @@
 
 
 // Things you need inorder to use std::move
-// 1. a
+// 1. a 
 template <class T, size_t R, size_t C>
 class array2d
 {
@@ -15,6 +15,7 @@ class array2d
     typedef value_type const *  const_iterator;
     std::vector<T> arr;
 public:
+    array2d():arr(R*C){}
     explicit array2d(std::initializer_list<T> list)
     {
         if(list.size() == R*C)
@@ -43,27 +44,30 @@ public:
         return arr.at((row-1)*C+col-1);
     }
 
-    constexpr T const & operator() (size_t const r, size_t const c) const
-    {
-        return arr[r*C + c];
-    }
+    // constexpr T const & operator() (size_t const r, size_t const c) const
+    // {
+    //     return arr[r*C + c];
+    // }
     
-    array2d fill(int i)
+    void fill(T i)
     {
-
+        for(auto& it : this->arr)
+        {
+            it = i;
+        }
     }
 
-    array2d swap(array2d input)
+    void swap(array2d& other) noexcept
     {
-
+        this->arr.swap(other.arr)
     }
 };
 
 int main()
 {
     // element access
-    array2d<int, 2,3> a{1,2,3,
-                        4,5,6};
+    array2d<int, 2,3> a({1,2,3,
+                        4,5,6});
     // std::cout << a(2,3) << std::endl;
     for(size_t i = 1; i <= a.size(1); i++)
     {
@@ -77,9 +81,15 @@ int main()
     // std::copy(std::begin(a),std::end(a),std::ostream_iterator<int>(std::cout, " "));
 
     // filling
-    // array2d<int,2,3> b;
-    // b.fill(1);
-
+    array2d<int,2,3> b;
+    b.fill(1);
+    for(size_t i = 1; i <= b.size(1); i++)
+    {
+        for(size_t j = 1; j <= b.size(2); j++)
+        {
+            std::cout << b(i,j) << ", ";
+        }
+    }
     // swapping
     // a.swap(b);
 
@@ -87,3 +97,19 @@ int main()
     // array2d<int, 2, 3> c(std::move(b));
     return 0;
 }
+
+
+/*  Morals of the story
+    1.  for the function fill, I first used the for loop like this:
+        for(auto it : this->arr)
+        {
+            it = i;
+        }
+        I tried it using either it or *it, neither one worked. I searched and found out the true way of coding this is:
+        for(auto& it : this->arr)
+        {
+            it = i;
+        }
+        It worked.
+    2.  
+*/
