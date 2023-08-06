9df3615138fc59debd39f17f70511a973af19b68
Mahdi Movahedi <m.movahedimonfared@gmail.com>
2023-08-06 21:40:05 +0330

    comment updated in p19
    Signed-off-by: Mahdi Movahedi <m.movahedimonfared@gmail.com>

--- p19_addRangeToContainer/main.cpp
+++ p19_addRangeToContainer/main.cpp
@@ -7,6 +7,7 @@ template <typename C, typename... Args>
 void addToContainer(C& container, Args&&... args)
 {
     (container.push_back(args),...); // it could be a recursive function like the last example
+    // But here Fold expressions was used 
     // the above line expands like: container.push_back(a1),(container.push_back(a2),(container.push_back(a3), container.push_back(a4)))
     // It means multiple calls of push_back method
 }
