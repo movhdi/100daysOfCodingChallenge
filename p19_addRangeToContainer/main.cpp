22745edca9cae298ac954eb2f758e29db3fc59f1
Mahdi Movahedi <m.movahedimonfared@gmail.com>
2023-08-06 21:36:25 +0330

    P19_addRangeToContainer added, these excercises should be done multiple time inorder to master the concept!
    Signed-off-by: Mahdi Movahedi <m.movahedimonfared@gmail.com>

---
+++ p19_addRangeToContainer/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <iterator>
+
+template <typename C, typename... Args>
+void addToContainer(C& container, Args&&... args)
+{
+    (container.push_back(args),...); // it could be a recursive function like the last example
+    // the above line expands like: container.push_back(a1),(container.push_back(a2),(container.push_back(a3), container.push_back(a4)))
+    // It means multiple calls of push_back method
+}
+
+
+
+int main()
+{
+    std::vector<int> arr({1,2,3,4,5});
+    std::copy(std::begin(arr),std::end(arr), std::ostream_iterator<int>(std::cout, " "));
+    std::cout << std::endl;
+    addToContainer(arr, 6,7,8);
+    std::copy(std::begin(arr),std::end(arr), std::ostream_iterator<int>(std::cout, " "));
+    return 0;
+}
