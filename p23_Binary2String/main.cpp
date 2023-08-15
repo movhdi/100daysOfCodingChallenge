2cb30bfd07a83a98dfecd2fa3230c776669c2b9d
Mahdi Movahedi <m.movahedimonfared@gmail.com>
2023-08-15 20:12:52 +0330

    p23 solved with minor changes to the books answer
    Signed-off-by: Mahdi Movahedi <m.movahedimonfared@gmail.com>

--- p23_Binary2String/main.cpp
+++ p23_Binary2String/main.cpp
@@ -5,10 +5,24 @@
 #include <algorithm>
 #include <array>
 #include <cassert>
+#include <sstream>
+#include <iomanip>
+
 template <typename Iter>
 std::string bytes_to_hexstr(Iter begin, Iter end, bool const uppercase_flag = false)
 {
-    
+    std::ostringstream oss;
+    if(uppercase_flag)
+    {
+        oss.setf(std::ios_base::uppercase);
+    }
+
+    for(;begin != end ; ++begin)
+    {
+        oss.fill('0');
+        oss << std::setw(2) << std::hex <<std::right <</*std::setfill('0') <<*/  static_cast<int>(*begin);
+    }
+    return oss.str();
 }
 
 
@@ -24,6 +38,7 @@ int main()
     std::vector<unsigned char> vec{0xBA, 0xAD, 0xF0, 0x0D};
     std::array<unsigned char, 6> arr{1,2,3,4,5,6};
     unsigned char buf[5] = {0x11, 0x22, 0x33, 0x44, 0x55};
+    // std::cout << bytes_to_hexstr(vec) << '\n';
     assert(bytes_to_hexstr(vec, true) == "BAADF00D");
     assert(bytes_to_hexstr(arr, true) == "010203040506");
     assert(bytes_to_hexstr(buf,true) == "1122334455");
