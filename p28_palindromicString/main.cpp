a7ec54668c2aeb6c029f6ae1942079003efcb6c6
Mahdi Movahedi <m.movahedimonfared@gmail.com>
2023-08-21 20:12:22 +0330

    p28 copied
    Signed-off-by: Mahdi Movahedi <m.movahedimonfared@gmail.com>

--- p28_palindromicString/main.cpp
+++ p28_palindromicString/main.cpp
@@ -10,15 +10,16 @@
 std::string longest_palindrome(std::string_view text)
 {
     size_t const len = text.size();
-
+    size_t maxlen = 1;
+    size_t longestbegin{};
     // the element at [i, j] indicates whether the substring from position i to j is a palindrome.
-    std::vector<bool> table;
+    std::vector<bool> table(len*len,false);
 
     // Requirement 1
     // setting all [i,i] elements true
     for(size_t i{}; i < len ; i++)
     {
-        table.push_back(true);
+        table[ i*len + i ] = true;
     }
     // Requirement 2
     // setting elements [i,i+1] true for two character palindromes (consecutive two identical characters)
@@ -27,21 +28,35 @@ std::string longest_palindrome(std::string_view text)
         if(text[i] == text[i+1])
         {
             table[i*len + i + 1] = true;
+            if(maxlen < 2 )
+            {
+                longestbegin = i;
+                maxlen = 2;
+            }
         }
     }
     // Requirement 3
     // setting the element at [i,j] to true if the
     // element at [i+1,j-1] is true and the characters on the positions i and j in the string are
     // also equal.
-    for()
+    for(size_t k{3}; k <= len ; k++ )
     {
-        for()
+        for(size_t j{}; j <= len - k ; j++)
         {
-
+            size_t i = j + k -1;
+            if(text[i] == text[j] && table[(j+1)*len + i - 1])
+            {
+                table[j*len + i] = true;
+                if(maxlen < k)
+                {
+                    longestbegin = j;
+                    maxlen = k;
+                }
+            }
         }
     }
     // returning garbage but without error (for now)
-    return std::string(text.substr(1,2));
+    return std::string(text.substr(longestbegin,maxlen));
 }
 
 int main()
