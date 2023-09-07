bae4a4fbfa610d7e712b2804a382a9e211acdc21
Mahdi Movahedi <m.movahedimonfared@gmail.com>
2023-09-07 08:02:04 +0330

    a new algorithm for p29 was added.
    Signed-off-by: Mahdi Movahedi <m.movahedimonfared@gmail.com>

--- p29_LicensePlateValidate/main.cpp
+++ p29_LicensePlateValidate/main.cpp
@@ -20,17 +20,33 @@ bool validate_license_plate_format(std::string_view str)
 std::vector<std::string> extract_license_plate_numbers(std::string str)
 {
     std::vector<std::string> result;
-    std::regex rx(R"(([A-Z]{3}-[A-Z]{2} \d{3,4})*)");
+    std::regex rx(R"(([A-Z]{3}-[A-Z]{2} \d{3,4}))");
     std::regex_iterator<std::string::iterator> rit ( str.begin(), str.end(), rx);
     std::regex_iterator<std::string::iterator> rend;
 
-    while (rit!=rend) {
-        std::cout << rit->str() << std::endl;
-        if((*rit)[1].matched) result.push_back(rit->str());
-        ++rit;
+    // while (rit!=rend) {
+    //     std::cout << rit->str() << std::endl;
+    //     if((*rit)[1].matched) result.push_back(rit->str());
+    //     ++rit;
+    // }
+    std::smatch sm;
+    while(std::regex_search(str, sm, rx,std::regex_constants::match_any))
+    {
+        std::cout << sm.str(0) << std::endl;
+        result.push_back(sm.str(0));
+        str = sm.suffix().str();
     }
+
+
+    // for(unsigned int i = 0 ; i < sm.size() ; i++)
+    // {
+    //     std::cout <<"match " << i << "(" << sm[i] << ")";
+    //     std::cout << "at position " << sm.position(i) << std::endl;
+    // }
     return result;
 }
+
+
 int main()
 {
     assert(validate_license_plate_format("ABC-DE 123"));
