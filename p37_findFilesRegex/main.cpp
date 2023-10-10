e58cffd647fe395408760549b44b66814a7646b6
Mahdi Movahedi <m.movahedimonfared@gmail.com>
2023-10-10 23:33:18 +0330

    p37 done
    Signed-off-by: Mahdi Movahedi <m.movahedimonfared@gmail.com>

---
+++ p37_findFilesRegex/main.cpp
@@ -0,0 +1,44 @@
+#include <filesystem>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <regex>
+#include <vector>
+
+
+std::vector<std::filesystem::directory_entry> find_file(std::filesystem::path const &path, const std::regex &re)
+{
+    std::vector<std::filesystem::directory_entry> db;
+    for (const auto &entry : std::filesystem::recursive_directory_iterator{path})
+    {
+        if (std::regex_search(entry.path().filename().string(), re))
+        {
+            db.emplace_back(entry);
+        }
+    }
+    return db;
+}
+
+
+int main(int argc, char *argv[])
+{
+    const auto path = std::filesystem::current_path();
+    // auto files = find_file(path, std::regex{"jpg"});
+
+
+    namespace fs = std::filesystem;
+    auto dir = fs::temp_directory_path();
+    auto pattern = R"(wct[0-9a-zA-Z]{3}\.tmp)";
+    auto files = find_file(path, std::regex(pattern));
+
+    // auto result = find_file(dir, pattern);
+    for (auto const &entry : files)
+    {
+        std::cout << entry.path().string() << std::endl;
+    }
+
+
+    (void)argc;
+    (void)argv;
+    return 0;
+}
