47e68734f9d091ddddb8b30c6ab581e9e2238727
Mahdi Movahedi <m.movahedimonfared@gmail.com>
2023-11-05 19:27:15 +0330

    proceeding with vector of function wrapper object
    Signed-off-by: Mahdi Movahedi <m.movahedimonfared@gmail.com>

--- LambdaFunctions_c++17/2_polymorphyLambda/main.cpp
+++ LambdaFunctions_c++17/2_polymorphyLambda/main.cpp
@@ -7,11 +7,35 @@
 
 static auto consumer(auto &container)
 {
-    return [&](auto value){container.pushback(value);};
+    return [&](auto value){container.push_back(value);};
+}
+
+static void printContainer(const auto &container)
+{
+    for(const auto it: container)
+    {
+        std::cout << it << ", ";
+    }
+    std::cout << std::endl;
 }
 
 int main()
 {
+    std::deque<int> deq;
+    std::vector<int> vec;
+    std::list<int> lst;
+    const std::vector<std::function<void(int)>> consumers{consumer(deq),consumer(vec),consumer(lst)};
+    for (int i = 0; i < 10; i++)
+    {
+        for (auto consumer : consumers)
+        {
+            consumer(i);
+        }
+    }
+    printContainer(deq);
+    printContainer(vec);
+    printContainer(lst);
+
 
     return 0;
 }
