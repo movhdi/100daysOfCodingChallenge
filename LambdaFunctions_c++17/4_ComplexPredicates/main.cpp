49c36f6e02622964ce41a1f03576459cde9b3f1c
Mahdi Movahedi <m.movahedimonfared@gmail.com>
2023-11-07 00:24:27 +0330

    format correction
    Signed-off-by: Mahdi Movahedi <m.movahedimonfared@gmail.com>

--- LambdaFunctions_c++17/4_ComplexPredicates/main.cpp
+++ LambdaFunctions_c++17/4_ComplexPredicates/main.cpp
@@ -1,8 +1,8 @@
-#include <iostream>
-#include <functional>
-#include <string>
-#include <iterator>
 #include <algorithm>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <string>
 
 static bool begins_with_a(const std::string &str)
 {
@@ -13,16 +13,19 @@ static bool ends_with_b(const std::string &str)
     return str.rfind("b") == str.length() - 1;
 }
 
-template<typename A, typename B, typename F>
+template <typename A, typename B, typename F>
 auto combine(F binary_Function, A a, B b)
 {
-    return [=](auto param){return binary_Function(a(param), b(param));};
+    return [=](auto param) { return binary_Function(a(param), b(param)); };
 }
 
 int main()
 {
-    auto a_star_b (combine(std::logical_and<>{},begins_with_a,ends_with_b));
-    std::copy_if(std::istream_iterator<std::string>{std::cin},{},std::ostream_iterator<std::string>{std::cout, ", "},a_star_b);
+    auto a_star_b(combine(std::logical_and<>{}, begins_with_a, ends_with_b));
+    std::copy_if(std::istream_iterator<std::string>{std::cin},
+                 {},
+                 std::ostream_iterator<std::string>{std::cout, ", "},
+                 a_star_b);
     std::cout << std::endl;
     return 0;
 }
