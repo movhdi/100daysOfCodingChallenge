7ad861c67724b1130f9429705183831e35d4c5b9
Mahdi Movahedi <m.movahedimonfared@gmail.com>
2023-11-14 01:03:11 +0330

    std::transform
    Signed-off-by: Mahdi Movahedi <m.movahedimonfared@gmail.com>

---
+++ STLalgorithmBasics/4_transformingContainers/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <sstream>
+
+int main()
+{
+    std::vector<int> vec{1, 2, 3, 4, 5};
+    std::transform(std::begin(vec), std::end(vec), std::ostream_iterator<int> {std::cout , ", "}, [](int i) ->int {return i*i;});
+    std::cout << '\n';
+    auto int_to_string([](int i){return std::to_string(i);});
+    auto int_to_string2([](int i){std::stringstream ss;ss << i << "^2 = " << i * i << '\n'; return ss.str();});
+    std::vector<std::string> vec2;
+    std::transform(std::begin(vec), std::end(vec), std::ostream_iterator<std::string>{std::cout},int_to_string2);
+    std::transform(std::begin(vec), std::end(vec), std::back_inserter<std::vector<std::string>>(vec2),int_to_string2);
+    std::cout << std::endl;
+    std::copy(std::begin(vec2), std::end(vec2), std::ostream_iterator<std::string>{std::cout});
+    std::cout << std::endl;
+    return 0;
+}
