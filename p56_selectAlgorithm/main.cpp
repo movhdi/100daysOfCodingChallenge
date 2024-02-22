48f28f49ba67f982567559be28a7e32fcf4f267c
Mahdi Movahedi <m.movahedimonfared@gmail.com>
2024-02-22 21:00:33 +0330

    improvment on type deduction
    Signed-off-by: Mahdi Movahedi <m.movahedimonfared@gmail.com>

--- p56_selectAlgorithm/main.cpp
+++ p56_selectAlgorithm/main.cpp
@@ -11,12 +11,25 @@ struct book
     std::string title; 
     std::string author;
 };
-
-template <typename T>
-auto select(std::vector<book> vec, std::function<T(book const &)> pred)
+/*
+                                                                              // -------------------making type from the result of callable (F&) Typr, calling (T const &) Type ---------------//
+                                                                                                      //---------F &----------------//-----------------T const &--------------//
+template <typename T,typename A, typename F, typename R = typename std::decay<typename std::result_of<typename std::decay<F>::type&(typename std::vector<T, A>::const_reference)>::type>::type>
+std::vector<R> select(std::vector<T,A> const & vec, F&& f)
 {
-    std::vector<T> result;
-    std::transform(std::begin(vec), std::end(vec), std::back_inserter(result), pred);    
+    std::vector<R> result;
+    std::transform(std::cbegin(vec), std::cend(vec), std::back_inserter(result), std::forward<F>(f));    
+    return result;
+}
+
+*/
+
+// My own minimalistic simple R type deduction  
+template<typename T, typename F, typename R = std::decay_t<std::result_of_t<F(T const &)>>>
+std::vector<R> select(std::vector<T> const & vec, F&& f)
+{
+    std::vector<R> result;
+    std::transform(std::cbegin(vec), std::cend(vec), std::back_inserter(result), std::forward<F>(f));    
     return result;
 }
 
@@ -27,6 +40,6 @@ int main()
         {102, "Op Amps for everyone", "Texas Instrument"},
         {103, "C++ Concurrency in Action", "Anthony Williams"}
     };
-    auto result = select<std::string>(books, [](book const & b){return b.title;});
+    auto result = select(books, [](book const & b){return b.title;});
     std::copy(std::begin(result), std::end(result), std::ostream_iterator<std::string>{std::cout, "\n"});
 }
