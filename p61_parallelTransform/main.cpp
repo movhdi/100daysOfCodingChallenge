7d4a47ade72f72c266550665be667864e794c75b
Mahdi Movahedi <m.movahedimonfared@gmail.com>
2024-04-25 11:54:32 +0330

    some changes in formatting
    Signed-off-by: Mahdi Movahedi <m.movahedimonfared@gmail.com>

--- p61_parallelTransform/main.cpp
+++ p61_parallelTransform/main.cpp
@@ -1,8 +1,8 @@
-#include <iostream>
-#include <thread>
 #include <algorithm>
-#include <vector>
+#include <iostream>
 #include <iterator>
+#include <thread>
+#include <vector>
 
 // signature for transform algorithm
 /*
@@ -10,7 +10,11 @@ ForwardIt2 transform(ForwardIt1 first1, ForwardIt1 last1, ForwardIt2 first2, Una
 */
 
 template <typename ForwardIt1, typename ForwardIt2, typename UnaryOp>
-ForwardIt2 parallel_transform(int concurrencyLevel, ForwardIt1 first1, ForwardIt1 last1, ForwardIt2 first2, UnaryOp&& unary_op)
+ForwardIt2 parallel_transform(int concurrencyLevel,
+                              ForwardIt1 first1,
+                              ForwardIt1 last1,
+                              ForwardIt2 first2,
+                              UnaryOp &&unary_op)
 {
     auto distance = std::distance(first1, last1);
     if (distance <= 10000)
@@ -24,14 +28,16 @@ ForwardIt2 parallel_transform(int concurrencyLevel, ForwardIt1 first1, ForwardIt
         auto begin = first1;
         auto end = last1;
         auto first2_temp = first2;
-        for (size_t i = 0; i < concurrencyLevel ; i++)
+        for (size_t i = 0; i < concurrencyLevel; i++)
         {
             auto last1_temp = std::next(begin, portion);
             if (std::distance(last1_temp, end) < portion)
             {
                 last1_temp = end;
             }
-            std::thread th([begin, last1_temp, first2_temp, &unary_op](){std::transform(begin, last1_temp, first2_temp, std::forward<UnaryOp>(unary_op));});
+            std::thread th([begin, last1_temp, first2_temp, &unary_op]() {
+                std::transform(begin, last1_temp, first2_temp, std::forward<UnaryOp>(unary_op));
+            });
             thread_pool.emplace_back(std::move(th));
             first2_temp = std::next(first2_temp, portion);
             begin = last1_temp;
@@ -51,20 +57,30 @@ int main()
     size_t ParallelQuantity = 50006;
     std::vector<int> vec1(ParallelQuantity);
     std::vector<int> vec2(vec1.size());
-    std::generate(std::begin(vec1), std::end(vec1), [](){static int i{}; return i++;});
+    std::generate(std::begin(vec1), std::end(vec1), []() {
+        static int i{};
+        return i++;
+    });
     auto t1 = std::chrono::system_clock::now();
-    parallel_transform(cores, std::begin(vec1), std::end(vec1), std::begin(vec2), [](int& i){return i*2;});
+    parallel_transform(cores, std::begin(vec1), std::end(vec1), std::begin(vec2), [](int &i) { return i * 2; });
     auto t2 = std::chrono::system_clock::now();
     auto tParallel = t2 - t1;
     // std::copy(std::begin(vec2), std::end(vec2), std::ostream_iterator<int>{std::cout, ", "});
     std::vector<int> vec1Prime(singleCoreQuant);
     std::vector<int> vec2Prime(vec1Prime.size());
-    std::generate(std::begin(vec1Prime), std::end(vec1Prime), [](){static int i{}; return i++;});
+    std::generate(std::begin(vec1Prime), std::end(vec1Prime), []() {
+        static int i{};
+        return i++;
+    });
     t1 = std::chrono::system_clock::now();
-    parallel_transform(cores, std::begin(vec1Prime), std::end(vec1Prime), std::begin(vec2Prime), [](int& i){return i*2;});
+    parallel_transform(cores, std::begin(vec1Prime), std::end(vec1Prime), std::begin(vec2Prime), [](int &i) {
+        return i * 2;
+    });
     t2 = std::chrono::system_clock::now();
     auto tSingleCore = t2 - t1;
-    std::cout << "Execution time for parallel processing of " << ParallelQuantity << " data is " << tParallel.count() << std::endl;
-    std::cout << "Execution time for parallel processing of " << singleCoreQuant << " data is " << tParallel.count() << std::endl;
+    std::cout << "Execution time for parallel processing of " << ParallelQuantity << " data is " << tParallel.count()
+              << std::endl;
+    std::cout << "Execution time for parallel processing of " << singleCoreQuant << " data is " << tParallel.count()
+              << std::endl;
     return 0;
 }
